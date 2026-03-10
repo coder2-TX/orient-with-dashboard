3b03eb5c213770a2a6d0920cf7e6f7b5d3ffdb4b
coder2 <coder2.travelx@gmail.com>
2026-03-10 13:26:26 +0300

    Editing the company location on the 'Contact Us' page map

--- resources/views/site/pages/contact/partials/section-3.blade.php
+++ resources/views/site/pages/contact/partials/section-3.blade.php
@@ -4,7 +4,7 @@
   <div class="oy-contact-map__inner">
     <iframe 
       class="oy-contact-map__iframe" 
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d245453.11186716947!2d44.17362021650893!3d15.334008226998638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1603cbfb3372c3d3%3A0x1601053b2665a044!2sSana&#39;a%2C%20Yemen!5e0!3m2!1sen!2s!4v1700000000000!5m2!1sen!2s" 
+      src="https://www.google.com/maps?q=75XV%2BPHG%20%D8%B4%D8%B1%D9%83%D8%A9%20%D8%A3%D9%88%D8%B1%D9%8A%D9%86%D8%AA%20%D9%8A%D9%85%D9%86%2C%20Zero%20St%2C%20Sanaa%2C%20Yemen&z=18&output=embed" 
       allowfullscreen="" 
       loading="lazy" 
       referrerpolicy="no-referrer-when-downgrade"
