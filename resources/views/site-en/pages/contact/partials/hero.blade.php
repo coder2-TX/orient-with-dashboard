202106dc2bfd397c605d59e45fcd013c02d82887
coder2 <coder2.travelx@gmail.com>
2026-07-16 14:49:33 +0300

    Fix English Contact hero layout and image

--- resources/views/site-en/pages/contact/partials/hero.blade.php
+++ resources/views/site-en/pages/contact/partials/hero.blade.php
@@ -3,21 +3,29 @@
 
 @php
   use App\Models\ContactHero;
-  use Illuminate\Support\Facades\Storage;
 
   $contactHero = ContactHero::activeContent();
 
-  $heroImage = filled($contactHero->hero_image)
-      ? Storage::disk('public')->url($contactHero->hero_image)
-      : asset('assets/images/main/hero/1.png');
+  $heroImage = asset('assets/images/main/hero_en/1.jpg');
 @endphp
 
-<section class="oy-hero oy-hero--contact" id="contact-hero" aria-label="Contact Hero">
+<section
+  class="oy-hero oy-hero--contact oy-hero--contact-en"
+  id="contact-hero"
+  dir="ltr"
+  aria-label="Contact Hero"
+>
   <div class="oy-hero__slides">
-    <div class="oy-hero__slide is-active" style="background-image:url('{{ $heroImage }}')"></div>
+    <div
+      class="oy-hero__slide is-active"
+      style="background-image: url('{{ $heroImage }}')"
+    ></div>
   </div>
 
-  <div class="oy-hero__overlay" aria-hidden="true"></div>
+  <div
+    class="oy-hero__overlay"
+    aria-hidden="true"
+  ></div>
 
   <div class="oy-hero__content">
     <div class="oy-hero__content-inner">
@@ -26,9 +34,18 @@
       </h1>
 
       <div class="oy-contact-hero__row oy-reveal oy-delay-2">
-        <span class="oy-hero__h2-icon" aria-hidden="true"></span>
-        <span class="oy-contact-hero__pre">{{ $contactHero->pre_en }}</span>
-        <div class="oy-hero__h3 oy-contact-hero__h3">{{ $contactHero->company_en }}</div>
+        <span
+          class="oy-hero__h2-icon"
+          aria-hidden="true"
+        ></span>
+
+        <span class="oy-contact-hero__pre">
+          {{ $contactHero->pre_en }}
+        </span>
+
+        <div class="oy-hero__h3 oy-contact-hero__h3">
+          {{ $contactHero->company_en }}
+        </div>
       </div>
     </div>
   </div>
